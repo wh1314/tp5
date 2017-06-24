802ab2f8340ee03bf1482d0dc7b23a57f0bcb231
wh1314 <243064119@qq.com>
2017-06-24 16:22:12 +0800

    Add files via upload

---
+++ application/admin/controller/index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return 'admin模块，index控制器  index方法';
+    }
+}
---
+++ application/index/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '我是INDEX模块，index控制器  index方法';
+    }
+
+    public function  ok()
+    {
+        return '我是INDEX模块，index控制器  OK方法';
+    }
+}
